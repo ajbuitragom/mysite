9a96e522faf89ff297e67db776b5c70f05749063
David <alexlinares@misena.edu.co>
2012-12-13 15:55:06 -0500

    Added the function for fields to the admin in file admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,8 @@
+#Se importa la base de datos con la tabla Poll
 from polls.models import Poll
 from django.contrib import admin
 
-admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+admin.site.register(Poll, PollAdmin)
