11d2953a9a11ec8c427f58100aab12fdb1492dd5
Cristian012 <cronnoss012@gmail.com>
2012-12-14 14:04:32 -0500

    subida Templates

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,8 @@ from django.http import HttpResponse, Http404
 
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p},RequestContext(request))
+    return render_to_response('polls/detail.html', {'poll': p},
+    	RequestContext(request))
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
